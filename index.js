ec31c49704483c29ba89e80d3d5ab794de7d6e41
Mellinda-alt <mellindawambui@gmail.com>
2024-11-30 16:43:17 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(locationPoint) {
+    if (locationPoint < 42) {
+        return 42 - locationPoint;
+        
+    } else {
+        return locationPoint - 42;
+    }
+    
+}
+
+function distanceFromHqInFeet(locationPoint) {
+    const block = distanceFromHqInBlocks(locationPoint);
+    const feetLong = 264;
+    return block * feetLong;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    const travel = Math.abs(start - destination) * 264;
+    return travel;
+}
+
+
+function calculatesFarePrice(start, destination) {
+    const travel = Math.abs(start - destination) * 264;
+
+    if (travel > 2500) {
+        return 'cannot travel that far';
+    } else if (travel > 2000) {
+        return 25;
+    } else if (travel > 400) {
+        return (travel - 400) * 0.02;
+    } else {
+        return 0;
+    }
+    
+}
